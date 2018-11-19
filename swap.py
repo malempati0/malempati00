d2a51b2db373ded238add08469272845d0ece890
malempati0 <45159387+malempati0@users.noreply.github.com>
2018-11-19 11:05:49 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,9 @@
+num1 =100;
+num2 =456;
+print("Value of num1 before swapping: ", num1)
+print("Value of num2 before swapping: ", num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("Value of num1 after swapping: ", num1)
+print("Value of num2 after swapping: ", num2)
