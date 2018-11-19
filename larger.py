5e1fe687080ec0770ff9e7fc8015f097d953e67a
malempati0 <45159387+malempati0@users.noreply.github.com>
2018-11-19 11:58:46 +0530

    Create larger.py

---
+++ larger.py
@@ -0,0 +1,23 @@
+def determine_max_value(num_one, num_two,num_three,num_four,num_five,num_six,num_seven,num_eight,num_nine,num_ten):
+    return max(num_one, num_two,num_three,num_four,num_five,num_six,num_seven,num_eight,num_nine,num_ten)
+num_one = 6;
+print(num_one)
+num_two = 8;
+print(num_two)
+num_three=9;
+print(num_three)
+num_four=5;
+print(num_four)
+num_five=3;
+print(num_five)
+num_six=2;
+print(num_six)
+num_seven=1;
+print(num_seven)
+num_eight=10;
+print(num_eight)
+num_nine=29;
+print(num_nine)
+num_ten=90;
+print(num_ten)
+print("The larger number is: " + str(determine_max_value(num_one, num_two,num_three,num_four,num_five,num_six,num_seven,num_eight,num_nine,num_ten)))
